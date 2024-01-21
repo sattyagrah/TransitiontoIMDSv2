e2f3e7967e5d5c97ef684b1e23338d02b63dead6
Kumar Satyanshu <kkumarsatyanshu2107@gmail.com>
2024-01-21 11:48:19 +0530

    final update

--- transition.sh
+++ transition.sh
@@ -1,37 +1,21 @@
 #!/bin/bash
 
-echo "Script running..."
-# List all the enabled AWS regions
-regions=$(aws ec2 describe-regions --query 'Regions[].RegionName' --output text)
-
-# Loop through each region
-for region in $regions; do
-
-    # Find the number of instance(s) having IMDSv1
-    length=$(aws ec2 describe-instances --region $region --filters "Name=metadata-options.http-tokens,Values=optional" --query 'length(Reservations[].Instances[].InstanceId)')
+# Function to loop through each region and convert IMDSv1 to IMDSv2.
+loopregions(){
+    length=$(aws ec2 describe-instances --region $1 --filters "Name=metadata-options.http-tokens,Values=optional" --query 'length(Reservations[].Instances[].InstanceId)')
 
     if [ $length -gt 0 ];
     then
         echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
-        echo "Region is : $region"
+        echo "Region is : $1"
         echo " "
         
-        # Get the list of instances having IMDSv1
-        lists=$(aws ec2 describe-instances --region $region --query 'Reservations[*].Instances[?MetadataOptions.HttpTokens== `optional`].[InstanceId]' --output text)
+        # IMDS optional instance list
+        lists=$(aws ec2 describe-instances --region $1 --query 'Reservations[*].Instances[?MetadataOptions.HttpTokens== `optional`].[InstanceId]' --output text)
         
         # Loop through each instance in the list
         for list in $lists; do
-            echo "------------------------------------------"
-            echo "Instance is : $list"
-            echo " "
-
-            # Enable IMDSv2
-            imds=$(aws ec2 modify-instance-metadata-options --instance-id $list --http-tokens required --region $region --http-endpoint enabled)
-            echo -e "IMDSv2 for Instance $list has been enabled in the region $region, snippet below -\n$imds"
-
-            echo "------------------------------------------";
-            echo " ";
-        
+            convertimds $1 $list
         done
         
         echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~";
@@ -39,7 +23,31 @@ for region in $regions; do
     else
         continue
     fi
+}
 
+
+# Function to covert IMDSv1 to IMDSv2
+convertimds(){
+    echo "------------------------------------------"
+    echo "Instance is : $2"
+    echo " "
+
+    imds=$(aws ec2 modify-instance-metadata-options --instance-id $list --http-tokens required --region $1 --http-endpoint enabled)
+    echo -e "IMDSv2 for Instance $2 has been enabled in the region $1, snippet below -\n$imds"
+
+    echo "------------------------------------------";
+    echo " ";
+}
+
+# Main script starts here 
+
+echo "Script running..."
+# Set your AWS region
+regions=$(aws ec2 describe-regions --query 'Regions[].RegionName' --output text)
+
+# Loop through each regions
+for region in $regions; do
+    loopregions $region
 done
 
 echo "Completed."
