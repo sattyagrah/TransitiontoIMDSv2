ef98dabaa6ba61fb8c4a985eac6f7f9d426c7ca1
Kumar Satyanshu <kkumarsatyanshu2107@gmail.com>
2024-01-21 14:55:19 +0530

    final update

--- transition.sh
+++ transition.sh
@@ -32,7 +32,7 @@ convertimds(){
     echo "Instance is : $2"
     echo " "
 
-    imds=$(aws ec2 modify-instance-metadata-options --instance-id $list --http-tokens required --region $1 --http-endpoint enabled)
+    imds=$(aws ec2 modify-instance-metadata-options --instance-id $2 --http-tokens required --region $1 --http-endpoint enabled)
     echo -e "IMDSv2 for Instance $2 has been enabled in the region $1, snippet below -\n$imds"
 
     echo "------------------------------------------";
